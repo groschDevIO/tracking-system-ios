913352a3ed7be3faf0a80bf7270f62fccd82fc6c
Egor Filatov <23egih23@gmail.com>
2015-10-02 16:24:38 +0300

    Sample C++ project init

---
+++ source/c++/Tracking Tools/Tracking Tools/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tracking Tools
+//
+//  Created by Yuri Groschovskiy on 02.10.15.
+//  Copyright (c) 2015 Palantir Technologies. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
